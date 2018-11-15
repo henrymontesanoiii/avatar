b2c673541be68f8ef96fd1ddd5b7e9dd9b004b1c
henman3 <henrymontesanoiii@gmail.com>
2018-11-15 10:37:55 -0500

    scaffolded

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NoMatch from "./pages/NoMatch";
+
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component= />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
