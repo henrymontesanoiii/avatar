1bbfbbb5d81b7770d2b1ef48f7f6fe0ee6eb6f18
henman3 <henrymontesanoiii@gmail.com>
2018-11-19 15:10:49 -0500

    Clock, Calculator, Backgrounds

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NoMatch from "./pages/NoMatch";
+import avatarClock from "./components/Clock/";
+import avatarCalculator from "./components/Calculator";
+import Nav from "./components/Nav";
+import wallpaper from "../src/wallpaper.json";
 
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component= />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+
+class App extends Component {
+  state = {
+    wallpaperImage: wallpaper[0].image,
+    wallpapers: wallpaper
+  }
+
+  
+
+  handleClicked = (id) => {
+    console.log(this.state.wallpaperImage);
+    console.log("clicked" + id);
+    console.log(this.state);
+    let chosenWallpaper = this.state.wallpapers.find(wallpaper => {
+      return id === wallpaper.id;
+    });
+    console.log(chosenWallpaper);
+    this.setState({ wallpaperImage: chosenWallpaper.image })
+   
+  }
+
+  render() {
+
+    document.body.style.backgroundImage = `url(${this.state.wallpaperImage})`;
+    document.body.style.backgroundSize = "cover";
+    
+    console.log(this.state);
+    return (
+      
+      <Router >
+        <div  >
+          <Nav handleClicked={this.handleClicked}/>
+          <div>
+            <Switch>
+              <Route exact path="/clock" component={avatarClock} />
+              <Route exact path="/calculator" component={avatarCalculator} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    )
+  }
+};
 
 export default App;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import wallpaper from "../../wallpaper.json";
+
+
+const Nav = (props) => (
+
+
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="/">
+      Nav
+    </a>
+    <div className="dropdown">
+      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Change Wallpaper
+  </button>
+      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a className="dropdown-item" key={wallpaper[1].id}
+          id={wallpaper[1].id}
+          name={wallpaper[1].name}
+          image={wallpaper[1].image}
+          onClick={()=> props.handleClicked(wallpaper[1].id)}>Winter</a>
+        <a className="dropdown-item" key={wallpaper[2].id}
+          id={wallpaper[2].id}
+          name={wallpaper[2].name}
+          image={wallpaper[2].image}
+          onClick={()=> props.handleClicked(wallpaper[2].id)}>Summer</a>
+        <a className="dropdown-item" key={wallpaper[3].id}
+          id={wallpaper[3].id}
+          name={wallpaper[3].name}
+          image={wallpaper[3].image}
+          onClick={()=> props.handleClicked(wallpaper[3].id)}>Autumn</a>
+          <a className="dropdown-item" key={wallpaper[4].id}
+          id={wallpaper[4].id}
+          name={wallpaper[4].name}
+          image={wallpaper[4].image}
+          onClick={()=> props.handleClicked(wallpaper[4].id)}>Spring</a>
+      </div>
+    </div>
+  </nav>
+)
+
+
+  ;
+
+export default Nav;
